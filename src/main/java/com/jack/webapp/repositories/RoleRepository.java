24ea61adb1afe2168db1730f4b3560b7fc8b4ead
Jacek K. <jacekkwiecinski99@gmail.com>
2024-01-16 20:11:59 +0100

    Add RoleRepository

--- src/main/java/com/jack/webapp/repositories/RoleRepository.java
+++ src/main/java/com/jack/webapp/repositories/RoleRepository.java
@@ -3,6 +3,8 @@ package com.jack.webapp.repositories;
 import com.jack.webapp.domain.entities.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RoleRepository extends JpaRepository<Role, Long> {
+import java.util.Optional;
 
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String name);
 }
