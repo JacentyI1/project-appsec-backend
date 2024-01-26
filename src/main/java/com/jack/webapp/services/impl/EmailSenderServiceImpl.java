bc0a316c8d749784e3d16dedf43c10244eed0e19
Jacek K. <jacekkwiecinski99@gmail.com>
2024-01-26 22:56:55 +0100

    Fixed sendEmail implementation. Not final push.

--- src/main/java/com/jack/webapp/services/impl/EmailSenderServiceImpl.java
+++ src/main/java/com/jack/webapp/services/impl/EmailSenderServiceImpl.java
@@ -2,19 +2,14 @@ package com.jack.webapp.services.impl;
 
 import com.jack.webapp.services.EmailSenderService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailSenderServiceImpl implements EmailSenderService {
-//    @Autowired
-//    private JavaMailSender javaMailSender;
-
     @Autowired
-    private Environment env;
+    private JavaMailSender javaMailSender;
 
     public void sendEmail(String toEmail, String subject, String body) {
         SimpleMailMessage message = new SimpleMailMessage();
@@ -22,12 +17,7 @@ public class EmailSenderServiceImpl implements EmailSenderService {
         message.setTo(toEmail);
         message.setText(body);
         message.setSubject(subject);
-        getJavaMailSender().send(message);
-    }
-
-
-    private JavaMailSender getJavaMailSender(){
-        return new JavaMailSenderImpl();
+        javaMailSender.send(message);
     }
 
 }
