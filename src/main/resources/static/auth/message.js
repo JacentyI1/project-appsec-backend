1e6f804547e2b0978d077946f80a4d765d08624e
Jacek K. <jacekkwiecinski99@gmail.com>
2024-01-26 22:41:33 +0100

    Change to React. Update soon

--- src/main/resources/static/auth/message.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function (){
-    fetch('/messages')
-        .then(response => response.json())
-        .then(data => {
-            data.forEach(message ={
-            //     ...............
-            })
-        }).catch(error => console.error('Error fetching message display', error));
-});
